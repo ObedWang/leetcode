a1ca0d8efd41dc740b70332083df22f256eb4604
wangeb <wangeb@data4truth.com>
2021-04-25 14:59:46 +0800

    TreeNode update

--- editor/cn/TreeNode.java
+++ editor/cn/TreeNode.java
@@ -21,4 +21,33 @@ public class TreeNode {
 
     public TreeNode() {
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    public static TreeNode construct(Integer[] array) {
+        if (array == null || array.length == 0) return null;
+        return construct(array, 0);
+    }
+
+    private static TreeNode construct(Integer[] array, int index) {
+        if (array[index] == null) return null;
+        TreeNode node = new TreeNode(array[index]);
+        int left = (index + 1) * 2 - 1, right = (index + 1) * 2;
+        if (left < array.length) node.left = construct(array, left);
+        if (right < array.length) node.right = construct(array, right);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(construct(new Integer[]{5,1,4,null,null,3,6}));
+        System.out.println(construct(new Integer[]{1}));
+        System.out.println(construct(new Integer[]{1,2}));
+    }
 }
