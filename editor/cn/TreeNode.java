3c1176d55a605441fc010b14c62c0cd9358b18d2
wangeb <wangeb@data4truth.com>
2021-04-25 15:29:15 +0800

    TreeNode update ConvertSortedArrayToBinarySearchTree

--- editor/cn/TreeNode.java
+++ editor/cn/TreeNode.java
@@ -1,5 +1,7 @@
 package editor.cn;
 
+import java.util.*;
+
 /**
  * @author : wangebie
  * @date : 2021/4/21 14:03
@@ -45,9 +47,39 @@ public class TreeNode {
         return node;
     }
 
+    public static Integer[] plant(TreeNode node) {
+        int level = countLevel(node);
+        int count = 0;
+        for (int i = 0; i < level; i++) {
+            count += Math.pow(2, i);
+        }
+        Integer[] ans = new Integer[count];
+        preOrder(ans, 0, node);
+        return ans;
+    }
+
+    private static void preOrder(Integer[] list, int index, TreeNode node) {
+        if (node == null) return;
+        list[index] = node.val;
+        preOrder(list, (index + 1) * 2 - 1, node.left);
+        preOrder(list, (index + 1) * 2, node.right);
+    }
+
+    private static int countNum(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + countNum(node.left) + countNum(node.right);
+    }
+
+    private static int countLevel(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(countLevel(node.left), countLevel(node.right)) + 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(construct(new Integer[]{5,1,4,null,null,3,6}));
-        System.out.println(construct(new Integer[]{1}));
-        System.out.println(construct(new Integer[]{1,2}));
+        System.out.println(Arrays.toString(plant(construct(new Integer[]{}))));
+        System.out.println(Arrays.toString(plant(construct(new Integer[]{1}))));
+        System.out.println(Arrays.toString(plant(construct(new Integer[]{1, 2}))));
+        System.out.println(Arrays.toString(plant(construct(new Integer[]{1, 2, 3}))));
+        System.out.println(Arrays.toString(plant(construct(new Integer[]{1, 2, null, null, 3}))));
     }
 }
